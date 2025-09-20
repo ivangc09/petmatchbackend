8ad393393c9b331ceb898d5ae5382a72f43a11d4
ivangc09 <garcia.cruz.ivan09@gmail.com>
2025-09-20 14:39:10 -0600

    Debug test

--- petmatchbackend/settings.py
+++ petmatchbackend/settings.py
@@ -12,7 +12,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
