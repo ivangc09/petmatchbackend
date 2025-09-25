6546d420a3564a0e420ca49e7ee42215fe58ed4a
ivangc09 <garcia.cruz.ivan09@gmail.com>
2025-09-24 19:28:26 -0600

    debug_sockets

--- petmatchbackend/asgi.py
+++ petmatchbackend/asgi.py
@@ -5,9 +5,10 @@ from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application() 
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
+from django.urls import path, re_path
 from chat.token_auth import TokenAuthMiddleware
 from chat.consumers import DMConsumer, NotificationConsumer
+from chat.debug_ws import DebugWS
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
@@ -15,6 +16,7 @@ application = ProtocolTypeRouter({
         URLRouter([
             path("ws/chat/u/<int:peer_id>/", DMConsumer.as_asgi()),
             path("ws/notifications/", NotificationConsumer.as_asgi()),
+            re_path(r"^ws/.*$", DebugWS.as_asgi()), 
         ])
     ),
 })
